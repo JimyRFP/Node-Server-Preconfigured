2a3deeb1eb05a0bbc3c8b3abaf3e930003e48c2a
Rafael Floriani Pinto <rafaelflorianipinto@MacBook-Air-de-Rafael.local>
2023-01-02 04:02:47 -0300

    added user model declarations

--- dist/database/models/User.d.ts
+++ dist/database/models/User.d.ts
@@ -1,4 +1,8 @@
 import { Model } from "sequelize";
 export declare class User extends Model {
+    email: string;
+    first_name?: string;
+    is_active: boolean;
+    password_hash: string;
     static init(sequelize: any): void;
 }
--- src/database/models/User.ts
+++ src/database/models/User.ts
@@ -2,6 +2,10 @@ import { Model } from "sequelize";
 import { DataTypes } from "sequelize";
 
 export class User extends Model{
+    declare email:string;
+    declare first_name?:string;
+    declare is_active:boolean;
+    declare password_hash:string;
     static init(sequelize:any){
         super.init({
             first_name:DataTypes.STRING,
