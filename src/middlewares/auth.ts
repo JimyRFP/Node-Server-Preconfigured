b0fafc16ac3602b1c1822fb0eb0e0504766cdc58
Rafael Floriani Pinto <rafaelflorianipinto@MacBook-Air-de-Rafael.local>
2024-01-01 22:00:31 -0300

    updated ts

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -2,16 +2,19 @@ import { User, updateUserLastAction, userIsLogged } from "../server";
 import { JSONResponse } from "../server";
 import { getUserSessionData } from "../server";
 import { sendIError } from "../utils/response";
-
+import { NextFunction, Request, Response } from "express"
+export interface RequestAuthenticated extends Request{
+    user:User,
+}
 export async function setUserDataMiddleware(req:any,res:any,next:any){
     if(!userIsLogged(req))
       return res.status(401).send(JSONResponse({},"User Must Be Logged"));
     try{
         const dealerEmail=getUserSessionData(req);
-        const user=await User.findOne({where:{email:dealerEmail,is_active:true}});
+        const user:User=await User.findOne({where:{email:dealerEmail,is_active:true}});
         if(!user)
           throw "Unknown user";
-        req.user={email:dealerEmail,id:user.id};
+        req.user=user;
         await updateUserLastAction(user);
         next();
      }catch(e){
