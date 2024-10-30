81ce224b38dae933645193ee32ddb830eefd094f
Rafael Floriani Pinto <rafaelflorianipinto@MacBook-Air-de-Rafael.local>
2024-10-30 02:47:10 -0300

    upv

--- dist/database/database.js
+++ dist/database/database.js
@@ -4,7 +4,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.dataBase = void 0;
-//@ts-ignore
 //@ts-nocheck
 const sequelize_1 = require("sequelize");
 const database_1 = __importDefault(require("./../settings/database/database"));
--- src/database/database.ts
+++ src/database/database.ts
@@ -1,5 +1,5 @@
-//@ts-ignore
 //@ts-nocheck
 import {Sequelize}  from "sequelize";
 import config from "./../settings/database/database";
+
 export const dataBase=new Sequelize(config);
