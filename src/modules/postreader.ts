22f0b670ad6601d5826e48e918d1d97766daf0cf
Rafael Floriani Pinto <rafaelflorianipinto@MacBook-Air-de-Rafael.local>
2026-01-22 19:41:35 -0300

    up serve limit

--- src/modules/postreader.ts
+++ src/modules/postreader.ts
@@ -1,5 +1,5 @@
 import  express  from "express";
 export function initPostReader(app:any){
-    app.use(express.json({limit:'3mb'}));
-    app.use(express.urlencoded({limit:'3mb',extended:true}));
+    app.use(express.json({limit:'1mb'}));
+    app.use(express.urlencoded({limit:'1mb',extended:true}));
 }
